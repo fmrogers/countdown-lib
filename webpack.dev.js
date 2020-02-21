89ee7500c1af53fa18bf7624a7828d0428371c76
Frederick M. Rogers <fmrogers@carbonium.no>
2020-02-21 22:28:58 +0100

    Updated webpack dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,26 +1,22 @@
-const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    dev: './src/dev/index.ts',
+    dev: './src/init-test.ts',
   },
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Countdown lib - Development',
     }),
-    new webpack.ContextReplacementPlugin(/moment[\\\/]locale$/, /^\.\/(nb)$/),
   ],
   module: {
     rules: [
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        exclude: ['/node_modules/', '/src/dev/'],
+        exclude: ['/node_modules/', '/src/countdown.ts'],
       },
     ],
   },
