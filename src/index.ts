71909061c0a7cdb5501e225129522265f8b4a135
Frederick M. Rogers <fmrogers@carbonium.no>
2020-03-01 01:14:58 +0100

    Math works. Limited scope by removing years and months

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export interface TimeRemaining {
   [index: string]: number;
-  years: number;
   days: number;
   hours: number;
   minutes: number;
@@ -9,7 +8,6 @@ export interface TimeRemaining {
 
 export default class Countdown {
   private timeRemaining: TimeRemaining = {
-    years: 0,
     days: 0,
     hours: 0,
     minutes: 0,
@@ -21,26 +19,34 @@ export default class Countdown {
   private interval = 0;
   private timeout = 1000;
   private difference = 0;
+  private isComplete = false;
 
   constructor(endDate?: string) {
     this.endDateTimeStamp = endDate ? Date.parse(endDate) : 0;
   }
 
-  public startInterval(): void {
-    //console.log("Zero Hour: ", this.isZeroHour(this.timeRemaining));
-
+  public startCounter(callback: (remainder: TimeRemaining, isComplete: boolean) => void): void {
     this.interval = setInterval(() => {
       const remainder = this.calculateCountDown();
-      console.log(remainder);
-      this.isZeroHour(remainder) && this.stopInterval(this.interval);
+      this.isZeroHour(remainder) && this.stopCounter(this.interval);
+      callback(remainder, this.isComplete);
     }, this.timeout);
   }
 
-  private stopInterval(interval: number): void {
-    clearInterval(interval);
+  public addLeadingZeros(value: number): string {
+    let stringValue = value.toString();
+    if (stringValue.length < 2) {
+      stringValue = `0${value}`;
+    }
+    return stringValue;
   }
 
-  public isZeroHour(remainder: TimeRemaining) {
+  private stopCounter(interval: number): void {
+    clearInterval(interval);
+    this.isComplete = true;
+  }
+
+  private isZeroHour(remainder: TimeRemaining) {
     const remainingValues = Object.keys(remainder).map((key) => remainder[key]);
     const isZero = remainingValues.reduce((accumulator, currentValue) => accumulator + currentValue);
     return isZero === 0 ? true : false;
@@ -48,41 +54,14 @@ export default class Countdown {
 
   private calculateCountDown(): TimeRemaining {
     this.currentDateTimeStamp = Date.parse(String(new Date()));
-    this.difference = Math.floor((this.endDateTimeStamp - this.currentDateTimeStamp) / 1000);
-    console.log('Difference: ', this.difference);
+    this.difference = this.endDateTimeStamp - this.currentDateTimeStamp;
 
-    if (this.difference > 0) {
-      if (this.difference >= 365.25 * 86400) {
-        // 365.25 * 24 * 60 * 60
-        this.timeRemaining.years = Math.floor(this.difference / (365.25 * 86400));
-        this.difference -= this.timeRemaining.years * 365.25 * 86400;
-      }
-
-      if (this.difference >= 86400) {
-        // 24 * 60 * 60
-        this.timeRemaining.days >= Math.floor(this.difference / 86400);
-        this.difference -= this.timeRemaining.days * 86400;
-      }
-
-      if (this.difference >= 3600) {
-        // 60 * 60
-        this.timeRemaining.hours >= Math.floor(this.difference / 3600);
-        this.difference -= this.timeRemaining.hours * 36000;
-      }
-
-      if (this.difference >= 60) {
-        // 60
-        this.timeRemaining.minutes = Math.floor(this.difference / 60);
-        this.difference -= this.timeRemaining.minutes * 60;
-      }
-
-      //if (this.difference  60) {
-      this.timeRemaining.seconds = this.difference ? this.difference : 0;
-      //} else {
-      //this.timeRemaining.seconds = 0;
-      //}
+    if (this.difference >= 0) {
+      this.timeRemaining.days = Math.floor(this.difference / (1000 * 60 * 60 * 24));
+      this.timeRemaining.hours = Math.floor((this.difference / (1000 * 60 * 60)) % 24);
+      this.timeRemaining.minutes = Math.floor((this.difference / 1000 / 60) % 60);
+      this.timeRemaining.seconds = Math.floor((this.difference / 1000) % 60);
     }
-
     return this.timeRemaining;
   }
 }
